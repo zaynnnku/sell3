6a79b67bc088cd97bf5d2cadb81a83539ab00554
zaynnnku <zaynntech420@gmail.com>
2025-01-01 00:49:41 +0700

    Add files via upload

---
+++ settings/xwby.js
@@ -0,0 +1,13 @@
+// /settings/xwby.js
+
+const profileSettings = {
+  name: "B S C", // Nama pengguna
+  description: "BioLink Studio Code!", // Deskripsi singkat
+  profilePicture: "/media/profile.jpg", // URL foto profil
+  links: [
+    { label: "Link 1", url: "https://www.biolink.fun" }, // Tambahkan link
+    { label: "Link 2", url: "https://www.biolink.fun" },
+    { label: "Link 3", url: "https://www.biolink.fun" },
+    { label: "Link 4", url: "https://www.biolink.fun" }
+  ]
+};
